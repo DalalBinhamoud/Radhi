2275ce319e42b6def7db332da81dc9115349924e
Rating <dalalbinhamoud@gmail.com>
2023-05-29 10:13:38 +0300

    Initial Commit

---
+++ Radhi/RadhiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadhiApp.swift
+//  Radhi
+//
+//  Created by Dalal Macbook on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RadhiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
